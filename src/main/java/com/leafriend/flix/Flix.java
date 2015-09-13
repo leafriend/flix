6cb8967150a62bba9937b7f85e39a2840ce4c35d
leafriend <leafriend@gmail.com>
2015-09-13 22:53:15 +0900

    Add main class

---
+++ src/main/java/com/leafriend/flix/Flix.java
@@ -0,0 +1,15 @@
+package com.leafriend.flix;
+
+import java.io.Console;
+
+public class Flix {
+
+    public static void main(String[] args) {
+
+        Console console = System.console();
+
+        console.printf("Hello, FLiX\n");
+
+    }
+
+}
