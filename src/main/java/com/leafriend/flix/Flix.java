d378263208d88967e710fbf6be587c1d4014c8ab
leafriend <leafriend@gmail.com>
2015-09-14 01:00:35 +0900

    Add error() stub & traverse()

--- src/main/java/com/leafriend/flix/Flix.java
+++ src/main/java/com/leafriend/flix/Flix.java
@@ -1,8 +1,19 @@
 package com.leafriend.flix;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Flix {
 
     private Options options;
+
+    private int directoryCount;
+
+    private int fileCount;
+
+    private List<File> files;
 
     public static void main(String[] args) {
 
@@ -14,10 +25,50 @@ public class Flix {
 
     public Flix(Options options) {
         this.options = options;
+        this.directoryCount = 0;
+        this.fileCount = 0;
+        this.files = new ArrayList<>();
     }
 
     public void run() {
 
+    }
+
+    public void traverse(File dir) throws IOException {
+        for (File file : dir.listFiles()) {
+            String path = file.getCanonicalPath();
+            if (file.isFile()) {
+                fileCount++;
+                files.add(file);
+                if (options.isVerbose()) {
+                    String name = file.getName();
+                    System.out.print(name);
+                    // try { Thread.sleep(100); } catch (InterruptedException e)
+                    // {
+                    // // TODO Auto-generated catch block e.printStackTrace(); }
+                    for (int i = 0; i < name.length(); i++)
+                        System.out.print("\b");
+                    for (int i = 0; i < name.length(); i++)
+                        System.out.print(" ");
+                    for (int i = 0; i < name.length(); i++)
+                        System.out.print("\b");
+                }
+            } else if (file.isDirectory()) {
+                directoryCount++;
+                if (options.isVerbose()) {
+                    System.out.print("\n");
+                    System.out.print(path + File.separator);
+                }
+                traverse(file);
+            } else {
+                error("Path '" + path + "' is not a file or directory");
+            }
+        }
+    }
+
+    private void error(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
 }
