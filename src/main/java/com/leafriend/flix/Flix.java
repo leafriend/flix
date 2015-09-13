26e5d43fc231aab44d6b2babe0fcbfab0766defa
leafriend <leafriend@gmail.com>
2015-09-14 00:54:10 +0900

    Add constructor with options

--- src/main/java/com/leafriend/flix/Flix.java
+++ src/main/java/com/leafriend/flix/Flix.java
@@ -4,11 +4,17 @@ import java.io.Console;
 
 public class Flix {
 
+    private Options options;
+
     public static void main(String[] args) {
 
         Console console = System.console();
 
         console.printf("Hello, FLiX\n");
+    public Flix(Options options) {
+        this.options = options;
+    }
+
 
     }
 
